c793959a949b718f399076a24ee00232a6e1257a
Kingsleymuturi <kingsleymuturi9@gmail.com>
2021-06-29 15:00:07 +0300

    Added BubbleSort algorithm

--- src/main/java/com/spotadev/algo/difficulty_5/sort/bubblesort/BubbleSort.java
+++ src/main/java/com/spotadev/algo/difficulty_5/sort/bubblesort/BubbleSort.java
@@ -13,7 +13,51 @@
 */
 package com.spotadev.algo.difficulty_5.sort.bubblesort;
 
-// @todo
+/**
+ * Implement a method with the following method signature:
+ * 
+ * public void bubbleSort(int[] array, int fromIndex, int toIndex)
+ * 
+ * The data in array should be sorted to a correct ascending or descending
+ * order. Create a testng unit test to prove your reverse works. Print both
+ * unsorted and sorted array to the console.
+ *
+ * Time Allocated: 20 minutes
+ *
+ * Difficulty: 5
+ * 
+ * @author Kingsley Muturi - 29th June 2021
+ */
+
 public class BubbleSort {
 
+	/*
+	 * bubble sort method taking in an array and two integers of the index to start
+	 * sorting from and index to which the array is to be sorted
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void bubbleSort(int[] array, int fromIndex, int toIndex) {
+		int d;
+
+		// for loop iterating the whole array again if elements aren't sorted yet
+		for (int i = toIndex - 1; i > fromIndex; i--) {
+			/*
+			 * boolean to help break the loop if the "if" in the for loop below is not
+			 * accesses as isSorted will remain true
+			 */
+			boolean isSorted = true;
+			for (int j = fromIndex; j < i; j++) {
+				// If consecutive elements in array are in wrong order then swap them!
+				if (((Comparable) array[j]).compareTo(array[j + 1]) > 0) {
+					isSorted = false;
+					d = array[j + 1];
+					array[j + 1] = array[j];
+					array[j] = d;
+				}
+			}
+			// If there is no swapping then array is already sorted!! Hence break!
+			if (isSorted)
+				break;
+		}
+	}
 }
---
+++ src/test/java/com/spotadev/algo/difficulty_5/sort/bubblesort/BubbleSortTest.java
@@ -0,0 +1,69 @@
+package com.spotadev.algo.difficulty_5.sort.bubblesort;
+
+import java.util.Arrays;
+
+/**
+ * Implement a method with the following method signature:
+ * 
+ *     public void bubbleSort(int[] array, int fromIndex, int toIndex)
+ * 
+ * The data in array should be sorted to a correct ascending order. Create a testng unit test to prove
+ * your reverse works. Print both unsorted and sorted array to the console.
+ *
+ * Time Allocated: 20 minutes
+ *
+ * Difficulty: 5
+ * 
+ * @author Kingsley Muturi - 29th June 2021
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class BubbleSortTest {
+	private Logger logger = LoggerFactory.getLogger(BubbleSortTest.class);
+	private BubbleSort bubbleSort;
+
+	// method to make output array to be readable and understandable
+	private String show(int[] array) {
+		StringBuilder sb = new StringBuilder("{");
+
+		for (int someInt : array) {
+
+			sb.append(someInt).append(", ");
+		}
+
+		sb.append("}");
+		return sb.toString();
+	}
+
+	@BeforeClass
+	public void setUp() {
+
+		bubbleSort = new BubbleSort();
+	}
+
+	@Test
+	public void bubbleSortTest() {
+
+		// This is unsorted array
+		int[] someArray = new int[] { 16, 13, 42, 11, 9, 6, 75, 40 };
+
+		// This is the expected array after using bubble sort method
+		int[] expected = new int[] { 6, 9, 11, 13, 16, 40, 42, 75 };
+
+		logger.info(show(someArray));
+
+		// sorting values in the array using the bubble sort method
+		bubbleSort.bubbleSort(someArray, 0, someArray.length);
+
+		// show the sorted array using bubble sort
+		logger.info(show(someArray));
+
+		// compare if the output is equal to the expected sorted array.
+		Assert.assertTrue(Arrays.equals(someArray, expected));
+	}
+}
