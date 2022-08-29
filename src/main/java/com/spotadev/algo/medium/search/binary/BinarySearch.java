05a9d223b88773dfc52edd5ca59795ed1b258977
karema99 <cavin.mulewa.karema@gmail.com>
2022-08-29 10:10:35 +0300

    Added implementation of binary search

--- src/main/java/com/spotadev/algo/medium/search/binary/BinarySearch.java
+++ src/main/java/com/spotadev/algo/medium/search/binary/BinarySearch.java
@@ -15,5 +15,28 @@ package com.spotadev.algo.medium.search.binary;
 
 // @todo
 public class BinarySearch {
+    public static int binarySearch(int[] arr, int value){
+        int mid;
+        int low = 0;
+        int high = arr.length - 1;
 
+        while (low <= high){
+            mid = low + (high - low) / 2;
+
+            if(arr[mid] ==value)
+                return mid;
+            else if (arr[mid] < value)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+
+    // Driver code
+    public static void main (String [] args) {
+        System.out.println("Binary search >>");
+        int[] arrNums = {10, 20, 30, 40, 50, 60};
+        System.out.println(binarySearch(arrNums, 90));
+    }
 }
