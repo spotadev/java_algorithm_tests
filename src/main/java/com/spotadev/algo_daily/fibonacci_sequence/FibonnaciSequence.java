19b40af7f1440587157dd589648738e409230ebb
Cavin Karema <cavin.mulewa.karema@gmail.com>
2022-08-30 03:04:39 +0300

    Added challenges to algodaily

---
+++ src/main/java/com/spotadev/algo_daily/fibonacci_sequence/FibonnaciSequence.java
@@ -0,0 +1,15 @@
+package com.spotadev.algo_daily.fibonacci_sequence;
+
+// Don't understand what the question requires as output
+/**
+ * PROBLEM STATEMENT:
+ * Implement a function that returns the Fibonnaci number for a given integer input. In a fibonnaci sequence, each sum
+ * is the sum of the two preceding numbers:
+ * Example of a fibonnaci series: 1, 1, 2, 3, 5, 8
+ *
+ * EXAMPLES:
+ *
+ *
+ */
+public class FibonnaciSequence {
+}
---
+++ src/main/java/com/spotadev/algo_daily/grab_random_node/GrabRandomNode.java
@@ -0,0 +1,17 @@
+package com.spotadev.algo_daily.grab_random_node;
+
+/**
+ * PROBLEM STATEMENT:
+ * Given a linked list, write a method to get a random node within it? Assuming you are given a random node generator.
+ * The linked list can have at least 2 nodes,  and may look something like this:
+ *  1 -> 2 -> 3 -> 4
+ *  The odds of getting any number between 1 and 4 inclusive should be exactly the same
+ *
+ * CONSTRAINTS:
+ * Length of the linked list <= 10000
+ * Nodes will always contain integer values between -1000000000 and 1000000000
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+public class GrabRandomNode {
+}
